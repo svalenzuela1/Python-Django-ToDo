97e96d0d0f324b949a91288e2f0b2d16dcc73e68
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-02-16 12:29:02 -0500

    adding jwt authentication

--- djangoToDo/settings.py
+++ djangoToDo/settings.py
@@ -80,6 +80,12 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
