6b016b64535752e561f6971b5ec6f2eb3938ed92
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-02-19 22:26:39 -0500

    adding path to URL

--- djangoToDo/urls.py
+++ djangoToDo/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/', include('authentication.urls')),
-
+    path('todo/', include('todo.urls')),
 ]
---
+++ todo/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from django.conf.urls import url
+from rest_framework import routers
+from .views import TaskViewSet, TaskItems, OneItem
+
+router = routers.DefaultRouter()
+router.register(r'tasks', TaskViewSet, basename='tasks')
+
+custom_urlpatterns = [
+    url(r'tasks/(?P<task_pk>\d+)/items$', TaskItems.as_view(), name='task_items'),
+    url(r'tasks/(?P<task_pk>\d+)/items/(?P<pk>\d+)$', OneItem.as_view(), name='task_item')
+]
+
+urlpatterns = router.urls + custom_urlpatterns
