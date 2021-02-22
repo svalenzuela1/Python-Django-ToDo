5965a49e8a2f3625c4c9d9c27fc826200a737f0c
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-02-21 23:56:32 -0500

    removing routers from url since authentication model is built

--- djangoToDo/urls.py
+++ djangoToDo/urls.py
@@ -20,16 +20,9 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from rest_framework import routers
-
-# router = routers.DefaultRouter()
-# router.register(r'registration', authenticationViews.RegistrationAPIView)
-# router.register(r'login', authenticationViews.LoginAPIView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/', include('authentication.urls')),
     path('tasks/', include('todo.urls')),
 ]
