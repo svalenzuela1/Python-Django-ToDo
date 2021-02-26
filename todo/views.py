05d1142256bf545e77f646357877071ac30ede1c
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-02-25 23:30:15 -0500

    removing unused variables from todo views

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets, status
-from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.exceptions import (ValidationError, PermissionDenied)
 from rest_framework.permissions import IsAuthenticated, AllowAny
