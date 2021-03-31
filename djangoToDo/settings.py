f33f98570999f9a80b82b2425d0286127c0284ba
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-03-31 16:49:14 -0400

    setting up middleware for authentication

--- djangoToDo/settings.py
+++ djangoToDo/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'authentication',
-    'todo'
+    'todo',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
